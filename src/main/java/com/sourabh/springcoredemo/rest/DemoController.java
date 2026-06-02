6a520a97dda535d644862f8ab8aeb92531a52c89
SourabhGupta15 <guptasourabh500@gmail.com>
2026-06-02 22:16:29 +0530

    Added code to demo Qualifier annotation using constructor injection.

--- src/main/java/com/sourabh/springcoredemo/rest/DemoController.java
+++ src/main/java/com/sourabh/springcoredemo/rest/DemoController.java
@@ -1,6 +1,7 @@
 package com.sourabh.springcoredemo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +14,16 @@ public class DemoController {
 	private Coach myCoach;
 
 	// define a constructor for dependency injection
-//	@Autowired
-//	public DemoController(Coach theCoach) {
-//		myCoach = theCoach;
-//	}
-	
-	// define a setter method for dependency injection
 	@Autowired
-	public void setCoach(Coach theCoach) {
+	public DemoController(@Qualifier("trackCoach") Coach theCoach) {
 		myCoach = theCoach;
 	}
+	
+	// define a setter method for dependency injection
+//	@Autowired
+//	public void setCoach(Coach theCoach) {
+//		myCoach = theCoach;
+//	}
 	
 	@GetMapping("/dailyworkout")
 	public String getDailyWork() {
