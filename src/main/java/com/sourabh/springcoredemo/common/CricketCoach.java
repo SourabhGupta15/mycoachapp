438c3a568015395efffd782f97902766883a91c3
SourabhGupta15 <guptasourabh500@gmail.com>
2026-06-04 00:01:07 +0530

    Added code to demo Lazy Initialization for TrackCoach class.

--- src/main/java/com/sourabh/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/sourabh/springcoredemo/common/CricketCoach.java
@@ -1,11 +1,14 @@
 package com.sourabh.springcoredemo.common;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+//@Primary
 public class CricketCoach implements Coach{
+	
+	public CricketCoach() {
+		System.out.println("In constructor: " + getClass().getSimpleName());
+	}
 
 	@Override
 	public String getDailyWorkout() {
