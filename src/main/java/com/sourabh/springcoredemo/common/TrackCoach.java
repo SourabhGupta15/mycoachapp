3b96f3dcea98aa80ddd5ac8a4bd0c2765d9a9d42
SourabhGupta15 <guptasourabh500@gmail.com>
2026-06-04 02:09:18 +0530

    Modified code to check whether the bean scope is Singleton.

--- src/main/java/com/sourabh/springcoredemo/common/TrackCoach.java
+++ src/main/java/com/sourabh/springcoredemo/common/TrackCoach.java
@@ -1,10 +1,9 @@
 package com.sourabh.springcoredemo.common;
 
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Lazy
+//@Lazy
 public class TrackCoach implements Coach {
 	
 	public TrackCoach() {
--- src/main/java/com/sourabh/springcoredemo/rest/DemoController.java
+++ src/main/java/com/sourabh/springcoredemo/rest/DemoController.java
@@ -12,12 +12,17 @@ public class DemoController {
 
 	// define a private field for dependency
 	private Coach myCoach;
+	
+	private Coach anotherCoach;
 
 	// define a constructor for dependency injection
 	@Autowired
-	public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+	public DemoController(
+			@Qualifier("cricketCoach") Coach theCoach,
+			@Qualifier("cricketCoach") Coach theAnotherCoach) {
 		System.out.println("In constructor: " + getClass().getSimpleName());
 		myCoach = theCoach;
+		anotherCoach = theAnotherCoach;
 	}
 	
 	// define a setter method for dependency injection
@@ -29,5 +34,10 @@ public class DemoController {
 	@GetMapping("/dailyworkout")
 	public String getDailyWork() {
 		return myCoach.getDailyWorkout();
+	}
+	
+	@GetMapping("/check")
+	public String check() {
+		return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
 	}
 }
